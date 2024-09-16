7853359848fdbcf3c96c7d41f13fd6a3fea9e1f5
Mingzhe <ou.mi@northeastern.edu>
2024-09-16 02:31:03 -0700

    Add 'Labs/Lab1'

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import Labs from './Labs';
+import Lab1 from './Labs/Lab1';
 
 function App() {
   return (
     <div>
       <h1>Welcome to Web Dev</h1>
       <h1>Labs</h1>
-      <h2>Lab 1</h2>
+      <Lab1 />
       <h2>Lab 2</h2>
       <h2>Lab 3</h2>
     </div>
