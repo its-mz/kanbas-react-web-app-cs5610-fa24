63c1726ea8b473d05a1982dee24df8df932d0e33
Mingzhe <ou.mi@northeastern.edu>
2024-09-16 03:21:00 -0700

    a1 2.8 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-//import Labs from './Labs';
+import Labs from './Labs';
 import Lab1 from './Labs/Lab1';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Web Dev</h1>
-      <h1>Labs</h1>
+      <Labs />
       <Lab1 />
       <h2>Lab 2</h2>
       <h2>Lab 3</h2>
--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -8,7 +8,18 @@ export default function Lab1() {
                 Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
                 <h4>Paragraphs</h4>
                 <p id="wd-p-1">
-                    This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag
+                    This is the first paragraph. The paragraph tag is used to format
+                    vertical gaps between long pieces of text like this one.
+                </p>
+                <p id="wd-p-2">
+                    This is the second paragraph. Even though there is a deliberate white
+                    gap between the paragraph above and this paragraph, by default
+                    browsers render them as one contiguous piece of text as shown here on
+                    the right.
+                </p>
+                <p id="wd-p-3">
+                    This is the third paragraph. Wrap each paragraph with the paragraph
+                    tag to tell browsers to render the gaps.
                 </p>
                 <div id="wd-lists">
                     <h4>List Tags</h4>
@@ -25,6 +36,24 @@ export default function Lab1() {
                         <li>Serve and enjoy!</li>
                     </ol>
                 </div>
+                <ol id="wd-my-favorite-revipe">
+                    <h5>Unordered List Tag</h5>
+                    My favorite books (in no particular order)
+                    <ul id="wd-my-books">
+                        <li>Dune</li>
+                        <li>Lord of the Rings</li>
+                        <li>Ender's Game</li>
+                        <li>Red Mars</li>
+                        <li>The Forever War</li>
+                    </ul>
+                    Your favorite books (in no particular order)
+                    <ul id="wd-your-books">
+                        <li>Harry Potter</li>
+                        <li>Bible</li>
+                        <li>Sherlock Holmes</li>
+                    </ul>
+                </ol>
+
                 <div id="wd-tables">
                     <h4>Table Tag</h4>
                     <table border={1} width="100%">	{/* table, border, width */}
