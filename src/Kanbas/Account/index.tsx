4c9be8d9fd35c3d4b82c8946ef38347bf4c55421
Mingzhe <ou.mi@northeastern.edu>
2024-09-17 01:13:19 -0700

    signin signup update

---
+++ src/Kanbas/Account/index.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router";
+import Profile from "./Profile";
+import Signin from "./Signin";
+import Signup from "./Signup";
+
+export default function Account() {
+    return (
+        <div id="wd-account-screen">
+            <h2>Account</h2>
+            <Routes>
+                <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+                <Route path="/Signin" element={<Signin />} />
+                <Route path="/Profile" element={<Profile />} />
+                <Route path="/Signup" element={<Signup />} />
+            </Routes>
+
+        </div>
+    );
+}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,7 +1,15 @@
-export default function Kanbas(){
+import { Navigate, Route, Routes } from "react-router";
+import Account from "./Account";
+
+
+export default function Kanbas() {
     return (
         <div id="wd-kanbas">
             <h1>Kanbas</h1>
+            <Routes>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="/Account/*" element={<Account />} />
+            </Routes>
         </div>
     );
 }
