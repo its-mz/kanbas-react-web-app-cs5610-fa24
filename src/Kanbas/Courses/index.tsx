14ca2944656c16087c1a38894623ecb4646a2f32
Mingzhe <ou.mi@northeastern.edu>
2024-09-18 17:47:34 -0700

    Done 3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -23,6 +23,10 @@ export default function Courses() {
                             <Route path="Assignments" element={<Assignments />} />
                             <Route path="Assignments/:aid" element={<AssignmentEditor />} />
                             <Route path="People" element={<h2>People</h2>} />
+                            <Route path="Zoom" element={<h2>Zoom</h2>} />
+                            <Route path="Piazza" element={<h2>Piazza</h2>} />
+                            <Route path="Grades" element={<h2>Grades</h2>} />
+                            <Route path="Quizzes" element={<h2>Quizzes</h2>} />
                         </Routes>
                     </td>
                 </tr>
--- src/Labs/Lab2.tsx
+++ src/Labs/Lab2.tsx
@@ -1,4 +1,4 @@
-export default function Labs(){
+export default function Lab2(){
     return (
         <div>
             <h2>Lab 2</h2>
