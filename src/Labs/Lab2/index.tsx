b0a59023165b03d85f03b16c7bc08de820f213e2
Mingzhe <ou.mi@northeastern.edu>
2024-09-27 02:16:51 -0700

    done 3.1.15

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,6 +2,10 @@ import "./index.css";
 import ForegroundColors from "./ForegroundColors";
 import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
+import Padding from "./Padding";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Positions from "./Positions";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -62,6 +66,14 @@ export default function Lab2() {
       <BackgroundColors />
 
       <Borders />
+
+      <Padding />
+
+      <Corners />
+
+      <Dimensions />
+
+      <Positions />
 
     </div>
   );
