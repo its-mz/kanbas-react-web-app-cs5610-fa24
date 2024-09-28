d175f22ccdb29fa5fee36c7861b10cbbecd3826e
Mingzhe <ou.mi@northeastern.edu>
2024-09-28 00:55:47 -0700

    update github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,16 +1,4 @@
 import { useLocation } from "react-router";
-// export default function TOC() {
-//   return (
-//     <ul>
-//       <li><Link to="/Labs">Labs</Link></li>
-//       <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-//       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-//       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-//       <li><Link to="/Kanbas">Kanbas</Link></li>
-//       <li><a href="https://github.com/its-mz/kanbas-react-web-app-cs5610-fa24/tree/a1" id="wd-github" target="_blank">GitHub</a><br /></li>
-//     </ul>
-//   );
-// }
 
 export default function TOC() {
   const { pathname } = useLocation();
@@ -45,7 +33,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
+        <a id="wd-k" href="https://github.com/its-mz/kanbas-react-web-app-cs5610-fa24/tree/a2" className="nav-link">
           My GitHub
         </a>
       </li>
