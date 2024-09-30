bdf9641944c4bf3a3717357ed8fdde34a4827123
Mingzhe <ou.mi@northeastern.edu>
2024-09-30 02:33:05 -0700

    done 4.1, starting 4.2

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -11,16 +11,11 @@ export default function Courses() {
         <div id="wd-courses">
             <h2 className="text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" />Course 1234</h2>
             <hr />
-            {/* <table>
-                <tr>
-                    <td valign="top"> */}
             <div className="d-flex">
                 <div className="d-none d-md-block">
                     <CoursesNavigation />
-                    {/* </td> */}
                 </div>
-                <div className="flex-fill">{/* </div> */}
-                    {/* <td valign="top"> */}
+                <div className="flex-fill">
                     <Routes>
                         <Route path="/" element={<Navigate to="Home" />} />
                         <Route path="Home" element={<Home />} />
@@ -33,9 +28,6 @@ export default function Courses() {
                         <Route path="Grades" element={<h2>Grades</h2>} />
                         <Route path="Quizzes" element={<h2>Quizzes</h2>} />
                     </Routes>
-                    {/* </td>
-                </tr>
-            </table> */}
                 </div>
             </div>
         </div >
