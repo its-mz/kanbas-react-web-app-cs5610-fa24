6bd6bc7fc7f8ba11db99f36f8eddc8c854fbdd61
Mingzhe <ou.mi@northeastern.edu>
2024-10-03 11:26:55 -0700

    test

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -44,46 +44,48 @@ export default function AssignmentEditor() {
                     </div>
 
                     <div className="row mb-3" id="wd-submission-type">
-                        <div className="col-sm-2 d-flex align-items-center jusity-content-end">
+                        <div className="col-md-2 d-flex align-items-start jusity-content-end">
                             <label htmlFor="wd-submission-type" className="form-label">Submission Type</label>
                         </div>
 
-                        <div className="border p-3 rounded mb-3 col-md-10" id="wd-submission-option">
-                            <div className="mb-3">
+                        <div className="p-3">
+                            <div className="border p-3 rounded mb-3 col-md-10" id="wd-submission-option">
+                                <div className="mb-3">
+                                    <div>
+                                        <select id="wd-group" className="form-select">
+                                            <option value="Online">Online</option>
+                                        </select>
+                                    </div>
+                                </div>
                                 <div>
-                                    <select id="wd-group" className="form-select">
-                                        <option value="Online">Online</option>
-                                    </select>
+                                    <label className="form-label pb-3"><b>Online Entry Options</b></label>
+                                </div>
+                                <div className="form-check mb-4" id="wd-text-entry">
+                                    <input type="checkbox" className="form-check-input" id="wd-text-entry" />
+                                    <label className="form-check-label" htmlFor="wd-text-entry">Text Entry</label>
+                                </div>
+                                <div className="form-check mb-4" id="wd-website-url">
+                                    <input type="checkbox" className="form-check-input" id="wd-website-url" />
+                                    <label className="form-check-label" htmlFor="wd-website-url">Website URL</label>
+                                </div>
+                                <div className="form-check mb-4" id="wd-media-recordings">
+                                    <input type="checkbox" className="form-check-input" id="wd-media-recordings" />
+                                    <label className="form-check-label" htmlFor="wd-media-recordingsi">Media Recordings</label>
+                                </div>
+                                <div className="form-check mb-4" id="wd-student-annotation">
+                                    <input type="checkbox" className="form-check-input" id="wd-student-annotation" />
+                                    <label className="form-check-label" htmlFor="wd-student-annotation">Student Annotation</label>
+                                </div>
+                                <div className="form-check mb-4" id="wd-file-upload">
+                                    <input type="checkbox" className="form-check-input" id="wd-file-upload" />
+                                    <label className="form-check-label" htmlFor="wd-file-upload">File Uploads</label>
                                 </div>
                             </div>
-                            <div>
-                                <label className="form-label pb-3"><b>Online Entry Options</b></label>
-                            </div>
-                            <div className="form-check mb-4" id="wd-text-entry">
-                                <input type="checkbox" className="form-check-input" id="wd-text-entry" />
-                                <label className="form-check-label" htmlFor="wd-text-entry">Text Entry</label>
-                            </div>
-                            <div className="form-check mb-4" id="wd-website-url">
-                                <input type="checkbox" className="form-check-input" id="wd-website-url" />
-                                <label className="form-check-label" htmlFor="wd-website-url">Website URL</label>
-                            </div>
-                            <div className="form-check mb-4" id="wd-media-recordings">
-                                <input type="checkbox" className="form-check-input" id="wd-media-recordings" />
-                                <label className="form-check-label" htmlFor="wd-media-recordingsi">Media Recordings</label>
-                            </div>
-                            <div className="form-check mb-4" id="wd-student-annotation">
-                                <input type="checkbox" className="form-check-input" id="wd-student-annotation" />
-                                <label className="form-check-label" htmlFor="wd-student-annotation">Student Annotation</label>
-                            </div>
-                            <div className="form-check mb-4" id="wd-file-upload">
-                                <input type="checkbox" className="form-check-input" id="wd-file-upload" />
-                                <label className="form-check-label" htmlFor="wd-file-upload">File Uploads</label>
-                            </div>
-                        </div>
+                        </div>    
                     </div>
 
                     <div className="row mb-3" id="wd-assign-to">
-                        <div className="col-md-2 d-flex align-items-center jusity-content-end">
+                        <div className="col-md-2 d-flex align-items-start jusity-content-end">
                             <label id="wd-assign-to" className="form-label">Assign</label>
                         </div>
                         <div className="border p-3 rounded mb-3 col-md-10" id="wd-assign-to-box">
