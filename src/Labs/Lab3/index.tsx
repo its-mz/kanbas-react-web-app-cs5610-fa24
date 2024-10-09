13c17535eb560e976ec8a2bbd75cac5595c47a26
Mingzhe <ou.mi@northeastern.edu>
2024-10-09 15:06:28 -0700

    start a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,16 @@
-export default function Lab3(){
+import BooleanVariable from "./BooleanVariables";
+import IfElse from "./IfElse";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+
+export default function Lab3() {
     return (
-        <div>
+        <div id="wd-lab3">
             <h2>Lab 3</h2>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariable />
+            <IfElse />
         </div>
     )
 }
